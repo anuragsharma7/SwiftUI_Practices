3edec448eebb8ba725146079864e90a397926efa
Anurag Sharma <anurag20c@gmail.com>
2020-12-25 21:18:19 +0530

    Initial Commit

---
+++ PracticeSwiftUI/PracticeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeSwiftUIApp.swift
+//  PracticeSwiftUI
+//
+//  Created by Anurag Sharma on 25/12/20.
+//
+
+import SwiftUI
+
+@main
+struct PracticeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
